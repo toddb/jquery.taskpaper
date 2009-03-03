0217cb2bbf355a772ebc9430d264e54601645079
toddb <todd@8wireunlimited.com>
2009-03-03 09:50:15 +0000

    key presses

--- src/taskpaper.control.item.js
+++ src/taskpaper.control.item.js
@@ -6,40 +6,67 @@
 		  $().items()
 		    .unbind('keypress')   // FIXME - only need this because .live isn't working  
 		    .bind('keypress', function(event){
-            $(event.target).__keypress(event.which, window.getSelection())
+		        var key = (event.which == '0') ? event.keyCode : event.which
+            $(event.target).__keypress(key)
 		        })
 		},
 				
-		__keypress: function(which, selection){
-      // console.log(which)
+		__keypress: function(which){
+		  
+		  var KEYS = $.fn.taskpaper.KEYS = {   // FIXME
+     		ENTER: 13,
+     		DELETE: 8,
+     		PROJECT: 58, // :
+     		TASK: 45,    // -
+     		TAG: 64,
+     		UP: 38,
+     		DOWN: 40
+     	  }  
+
       switch(which)
       {
-        case 58:  // : project
+        case KEYS.PROJECT:
           if (this.isAtEndOfItem()) this.addClass("project")
           break;    
-        case 8:   // delete when at the start of an item remove 
-          if (this.isAtStartOfEmptyItem()) this.removeItem() 
+        case KEYS.DELETE:
+          if (this.isAtStartOfEmptyItem()) this.removeItem()
           if (this.isAtEndOfItem()) this.removeClass("project")          
           break;    
-        case 45:  // - task
-          if (this.isAtStartOfEmptyItem()) this.addClass("task") // || checkToRemoveProject()
+        case KEYS.TASK:
+          if (this.isAtStartOfEmptyItem()) this.addClass("task")
           break;
-        case 13:  // enter
-         this.appendItemToItem() // || this.appendItemToProject()
+        case KEYS.ENTER:
+          if (this.isAtStartOfEmptyItem()) return false
+          // create project and add task // if project
+          this.appendItemToItem() // && parse line to add tags // if task
           // check that the preceeding character is not a : because then it needs to leave the previous line as project!
           break;
+        case KEYS.UP:
+          if (this.parent().is(':first-child')) {
+            this.projectForItem().focus() // FIX ME
+          } else {
+            this.prevItem().focus()           
+          };
+          break;
+        case KEYS.DOWN:
+          if (this.parent().is(':last-child')) {
+            this.nextProject().focus()  // FIX ME
+          } else {
+            this.nextItem().focus()
+          };
+          break;
         default:
-      }		  
+      }		
+      
 		},
 		
 		appendItemToItem: function() {
 		  var span = $('<span contenteditable="true">')
             .addClass('task')
             .html('&nbsp;')     
-		  var item = $('<li>').append(span).insertAfter(this.parent(':first')).hide().slideDown('slow')
-		  // focus must come last
-      span.focus()
+		  var item = $('<li>').append(span).insertAfter(this.parent(':first')).hide().slideDown('slow')		  
       this.bindItems()
+      span.focus() // span focus must come after item creation
       return item
 		},
 		
@@ -66,7 +93,7 @@
 		},
 		
 		isAtStartOfEmptyItem: function() {  
-      return ($.trim($.selection().anchorNode.data).length == 0 && this.isInItem()) ? true : false
+      return ($.trim($.selection().focusNode.data).length == 0 && this.isInItem()) ? true : false
 		},
 				
 		isAtEndOfItem: function() {
@@ -80,13 +107,19 @@
     nextItem: function() {
       return this.parent().next().find('span')
     },
+
+    nextProject: function() {
+      return this.parent().next().find('li')  // FIX ME
+    },
+    
+    prevProject: function() {
+      return this.parent().next().find('li') // FIX ME
+    },
     
     projectForItem: function() {
       return this.parent().parent().parent().find('span.project')
     },
-    
-    
-
-		
+    		
   })
+  
 })
